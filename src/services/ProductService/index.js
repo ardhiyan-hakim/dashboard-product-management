f48087fbb42fd2b6dee26e019dbac49ac744b111
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2025-03-12 13:22:23 +0700

    Adding Method for Add, Update, and Delete Product for API

--- src/services/ProductService/index.js
+++ src/services/ProductService/index.js
@@ -11,3 +11,33 @@ export const fetchProducts = async () => {
     return [];
   }
 };
+
+export const addProduct = async (productData) => {
+  try {
+    const response = await axios.post(`${API_URL}/add`, productData);
+    return response.data;
+  } catch (error) {
+    console.error("Error adding product:", error);
+    return null;
+  }
+};
+
+export const updateProduct = async (id, updatedData) => {
+  try {
+    const response = await axios.put(`${API_URL}/${id}`, updatedData);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating product:", error);
+    return null;
+  }
+};
+
+export const deleteProduct = async (id) => {
+  try {
+    await axios.delete(`${API_URL}/${id}`);
+    return true;
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    return false;
+  }
+};
